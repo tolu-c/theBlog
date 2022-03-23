25f079bee1cdf074e352f8848f100c04ae65b43f
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-03-23 01:28:04 +0100

    added pagination and class based views. Done with chapter 1

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# class based views
+from django.views.generic import ListView
 
 # Create your views here.
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
+
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+    # print(page)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
 
 
 def post_detail(request, year, month, day, post):
