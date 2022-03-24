d3e5b0da190de7988459c84144148080cb27334d
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-03-24 11:43:16 +0100

    settings

--- theBlog/settings.py
+++ theBlog/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'theBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # postgresql
 DATABASES = {
     'default': {
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
